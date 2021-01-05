164233502ee592f5b531640ba84b44207cee3dc5
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-01-05 23:11:45 +0530

    Added Tests For Adding Todos

--- cypress/integration/baisc.spec.js
+++ cypress/integration/baisc.spec.js
@@ -13,14 +13,52 @@ describe("Basic Tests", () => {
     })
 })
 
-describe("Add A Todo", () => {
+describe("Add An Empty Todo", () => {
 
     it("opens the app", () => {
 
         cy.visit("http://localhost:3000")
-        cy.get("[data-test-id='todo-list'")
+        cy.get("[data-test-id='todo-add'")
+            .click()
 
-        cy.get("[data-test-id='todo-heading'")
-            .contains("Todo List")
+        cy.get("[data-test-class='todo-item'")
+            .should('have.length',0)
+    })
+})
+
+describe("Add An  Todo with content", () => {
+
+    it("opens the app", () => {
+
+        cy.visit("http://localhost:3000")
+
+        cy.get("[data-test-id='todo-input'")
+            .type("Task 1")
+        cy.get("[data-test-id='todo-add'")
+            .click()
+
+        cy.get("[data-test-class='todo-item'")
+            .should('have.length',1)
+    })
+})
+
+describe("Add Two Todo with content", () => {
+
+    it("opens the app", () => {
+
+        cy.visit("http://localhost:3000")
+
+        cy.get("[data-test-id='todo-input'")
+            .type("Task 1")
+        cy.get("[data-test-id='todo-add'")
+            .click()
+
+        cy.get("[data-test-id='todo-input'")
+            .type("Task 2")
+        cy.get("[data-test-id='todo-add'")
+            .click()
+
+        cy.get("[data-test-class='todo-item'")
+            .should('have.length',1)
     })
 })

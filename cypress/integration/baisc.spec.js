bbde2802170e00541cf264cf68811a793c6e1534
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-01-06 22:28:32 +0530

    Added Commands getByTestId And getByTestClass

--- cypress/integration/baisc.spec.js
+++ cypress/integration/baisc.spec.js
@@ -6,9 +6,9 @@ describe("Basic Tests", () => {
     it("opens the app", () => {
 
         cy.visit("http://localhost:3000")
-        cy.get("[data-test-id='todo-list'")
+        cy.getByTestId("todo-list")
 
-        cy.get("[data-test-id='todo-heading'")
+        cy.getByTestId("todo-heading")
             .contains("Todo List")
     })
 })
@@ -18,10 +18,10 @@ describe("Add An Empty Todo", () => {
     it("opens the app", () => {
 
         cy.visit("http://localhost:3000")
-        cy.get("[data-test-id='todo-add'")
+        cy.getByTestId("todo-add")
             .click()
 
-        cy.get("[data-test-class='todo-item'")
+        cy.getByTestClass("todo-item")
             .should('have.length',0)
     })
 })
@@ -32,12 +32,12 @@ describe("Add An  Todo with content", () => {
 
         cy.visit("http://localhost:3000")
 
-        cy.get("[data-test-id='todo-input'")
+        cy.getByTestId("todo-input")
             .type("Task 1")
-        cy.get("[data-test-id='todo-add'")
+        cy.getByTestId("todo-add")
             .click()
 
-        cy.get("[data-test-class='todo-item'")
+        cy.getByTestClass("todo-item")
             .should('have.length',1)
     })
 })
@@ -48,17 +48,17 @@ describe("Add Two Todo with content", () => {
 
         cy.visit("http://localhost:3000")
 
-        cy.get("[data-test-id='todo-input'")
+        cy.getByTestId("todo-input")
             .type("Task 1")
-        cy.get("[data-test-id='todo-add'")
+        cy.getByTestId("todo-add")
             .click()
 
-        cy.get("[data-test-id='todo-input'")
+        cy.getByTestId("todo-input")
             .type("Task 2")
-        cy.get("[data-test-id='todo-add'")
+        cy.getByTestId("todo-add")
             .click()
 
-        cy.get("[data-test-class='todo-item'")
+        cy.getByTestClass("todo-item")
             .should('have.length',2)
     })
 })
